25f282ee0ab9d8fc43247fc96f1f31bdd3790c07
Adam Meza <Adammeza95@gmail.com>
2022-12-05 14:41:46 -0500

    Answer questions 13,14

--- variables.js
+++ variables.js
@@ -14,3 +14,11 @@ console.log(capo)
 console.log(gabagool)
 console.log(emmys)
 
+capo = "Junior"
+goon1 = "Silvio"
+seasons = 6.5
+gabagool = false
+
+console.log(capo)
+console.log(goon1)
+console.log(gabagool)
